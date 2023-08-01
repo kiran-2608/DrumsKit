2e5ef36153c8a3cb0677986aa7822c2f71548d32
kiran-2608 <141059641+kiran-2608@users.noreply.github.com>
2023-08-01 10:49:33 +0530

    This is my second commit for this project

---
+++ index (3).php
@@ -0,0 +1,105 @@
+<?php
+    $insert = false;
+    if (isset($_POST['name'])) {
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+
+        $conn = mysqli_connect($server, $username, $password);
+
+        if (!$conn) {
+            die("Connection to this database failed due to " . mysqli_connect_error());
+        }
+        echo "Success connecting to the database";
+
+        $name = $_POST['name'];
+        $gender = $_POST['gender'];
+        $age = $_POST['age'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $description = $_POST['description'];
+        $sql = "INSERT INTO `trip` 'trip' (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$description', current_timestamp());";
+
+        if ($conn->query($sql) === true) {
+            $insert = true;
+        } else {
+            echo "Error: $sql <br> " . $conn->error;
+        }
+        $conn->close();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Form</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="svit">
+    <div class="container">
+        <h1>Welcome to Svit, Industrial visit form</h1>
+        <p>Enter your details and submit this form to confirm your participation in the trip</p>
+        <?php
+            if ($insert) {
+                echo '<p class="submitMsg">Thanks for submitting your form. We are happy to see you joining us for the industrial visit trip</p>';
+            }
+        ?>
+
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="number" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone number">
+            <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn">Submit</button>
+            <!-- <button class="btn">Reset</button> -->
+        </form>
+    </div>
+    <script src="index.js"></script>
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+</html>
